8ae5ce1c77b819dc8057f15b5851fc522afc67ed
OEM Configuration (temporary user) <d.shalabayev@kazdream.kz>
2023-09-26 16:39:46 +0600

    Checking

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 )
 
@@ -19,89 +18,45 @@ type SectorResponse struct {
 	Reason     string    `json:"reason"`
 }
 
-var finishedLocationsChannel chan map[string]int
-
-func ProcessedSectors() {
-	//uniqueSectorIds := make(map[int]struct{})
-	var selectedMessages []SectorResponse
-	//taskId := 1
-	processedMessages := 0
-	timeoutCount := 0
-	//round := 1
-	countSectors := 10
-	ch := make(chan int)
+func ProcessedSectors(fch chan map[string]int) {
 	FinishedLocations := make(map[string]int)
 
-	// Горутина для отправки сообщений в канал с паузой 2 секунды
-	go func() {
-		for i := 0; i < 10; i++ {
-			ch <- i
-			time.Sleep(2 * time.Second)
-
+	for i := 0; i < 10; i++ {
+		msg := i
+		if msg == 0 {
+			FinishedLocations["132-221"] = 1
+		} else if msg == 1 {
+			FinishedLocations["132-222"] = 2
+		} else if msg == 2 {
+			FinishedLocations["132-223"] = 3
+		} else if msg == 3 {
+			FinishedLocations["132-224"] = 4
+		} else if msg == 4 {
+			FinishedLocations["132-225"] = 5
+		} else if msg == 5 {
+			FinishedLocations["132-226"] = 6
+		} else if msg == 6 {
+			FinishedLocations["132-227"] = 7
+		} else if msg == 7 {
+			FinishedLocations["132-228"] = 8
+		} else if msg == 8 {
+			FinishedLocations["132-229"] = 9
+		} else if msg == 9 {
+			FinishedLocations["132-230"] = 10
 		}
-		close(ch)
-	}()
-
-loop:
-	for {
-		select {
-
-		case msg, ok := <-ch:
-			processedMessages++
-			fmt.Println("Проверка секторов")
-			if !ok {
-				log.Println("Channel closed. Exiting the loop.")
-
-				break loop
-			} else {
-				if msg == 0 {
-					FinishedLocations["132-221"] = 1
-				} else if msg == 1 {
-					FinishedLocations["132-222"] = 2
-				} else if msg == 2 {
-					FinishedLocations["132-223"] = 3
-				} else if msg == 3 {
-					FinishedLocations["132-224"] = 4
-				} else if msg == 4 {
-					FinishedLocations["132-225"] = 5
-				} else if msg == 5 {
-					FinishedLocations["132-226"] = 6
-				} else if msg == 6 {
-					FinishedLocations["132-227"] = 7
-				} else if msg == 7 {
-					FinishedLocations["132-228"] = 8
-				} else if msg == 8 {
-					FinishedLocations["132-229"] = 9
-				} else if msg == 9 {
-					FinishedLocations["132-230"] = 10
-				}
-				fmt.Println(msg)
-
-			}
-
-			// Ваш код обработки сообщения
-			// ...
-
-		case <-time.After(10 * time.Second):
-			if countSectors == len(selectedMessages) {
-				break loop
-			} else {
-				timeoutCount++
-				if timeoutCount == 10 {
-					break loop
-				}
-			}
-		}
-
+		time.Sleep(1 * time.Second)
+		fmt.Println("Проверка сектора", msg)
 	}
-	fmt.Println(FinishedLocations)
-	log.Println("Success sectors check, continue...")
+
+	fch <- FinishedLocations
+	close(fch)
 }
 
 func main() {
-	ProcessedSectors()
-	fmt.Println("Побежали сектора")
-
+	fch := make(chan map[string]int)
+	go ProcessedSectors(fch)
+	receivedMap := <-fch
+	fmt.Println("Отправка секторов в cond response")
 	Locations := make(map[string]int)
 	Locations["132-221"] = 1
 	Locations["132-222"] = 2
@@ -113,7 +68,20 @@ func main() {
 	Locations["132-228"] = 8
 	Locations["132-229"] = 9
 	Locations["132-230"] = 10
-	for _, item := range Locations {
-		fmt.Println(item)
+	Locations["132-231"] = 11
+	Locations["132-232"] = 12
+
+	errorLocation := make(map[string]int)
+
+	for key, value := range Locations {
+		if _, ok := receivedMap[key]; ok {
+
+		} else {
+			errorLocation[key] = value
+		}
+	}
+
+	for _, value := range errorLocation {
+		fmt.Println("Ошибочный сектор", value)
 	}
 }
