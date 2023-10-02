d80e2b18fe2cc1201f3e49ac3c3db5dff21de171
OEM Configuration (temporary user) <d.shalabayev@kazdream.kz>
2023-10-02 13:40:01 +0600

    Publish

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.19
+
+require github.com/streadway/amqp v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,62 +1,138 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/streadway/amqp"
 )
 
 type SectorResponse struct {
-	TaskID     int       `json:"task_id"`
-	RoundNo    int       `json:"round_no"`
-	SectorID   int       `json:"sector_id"`
-	CellID     int       `json:"cell_id"`
-	LAC        int       `json:"lac"`
-	Service    string    `json:"service"`
-	Timestamp  time.Time `json:"timestamp"`
-	ReturnCode int       `json:"return_code"`
-	Result     string    `json:"result"`
-	Reason     string    `json:"reason"`
+	TaskID     int
+	RoundNo    int
+	SectorID   int
+	CellID     int
+	LAC        int
+	Service    string
+	Timestamp  time.Time
+	ReturnCode int
+	Result     string
+	Reason     string
 }
 
-func ProcessedSectors(fch chan SectorResponse) {
+func Fill(ch *amqp.Channel) {
+	queueName := "processed_sectors"
+
 	for i := 0; i < 10; i++ {
 		msg := i
-		fmt.Println("Проверка сектора", msg)
+		fmt.Println("Sending sector", msg)
 		response := SectorResponse{
 			TaskID:     msg,
-			RoundNo:    msg,
+			RoundNo:    0,
 			SectorID:   msg,
 			CellID:     msg,
 			LAC:        msg,
 			Service:    "service",
 			Timestamp:  time.Now(),
-			ReturnCode: msg,
+			ReturnCode: 0,
 			Result:     "result",
 			Reason:     "reason",
 		}
-		fch <- response
-		time.Sleep(1 * time.Second)
 
+		jsonBody, err := json.Marshal(response)
+		if err != nil {
+			fmt.Println("Failed to marshal JSON:", err)
+			return
+		}
+
+		err = ch.Publish(
+			"",
+			queueName,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
+			})
+		if err != nil {
+			fmt.Println("Failed to publish a message:", err)
+			return
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func ConsumeMessages(ch *amqp.Channel) {
+	queueName := "processed_sectors"
+	msgs, err := ch.Consume(
+		queueName,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("Failed to register a consumer: %v", err)
 	}
 
-	close(fch)
+	timeout := time.After(30 * time.Second)
+	countSectors := 10
+	selectedMessages := make([]SectorResponse, 0)
+loop:
+	for {
+		select {
+		case msg, ok := <-msgs:
+			if !ok {
+				log.Println("Channel closed. Exiting the loop.")
+				return
+			}
+
+			var sectorResp SectorResponse
+			err := json.Unmarshal(msg.Body, &sectorResp)
+			if err != nil {
+				log.Println("Failed to unmarshal JSON:", err)
+				continue
+			}
+
+			if sectorResp.ReturnCode == 0 {
+				fmt.Println("Ack")
+				selectedMessages = append(selectedMessages, sectorResp)
+			}
+
+			if len(selectedMessages) == countSectors {
+				fmt.Println("Received all sectors.")
+				break loop
+			}
+
+		case <-timeout:
+			fmt.Println("Timed out. Exiting the loop.")
+			break loop
+		}
+	}
 }
 
 func main() {
-	fch := make(chan SectorResponse)
-	go ProcessedSectors(fch)
-	receivedSlice := []SectorResponse{}
-	fmt.Println("Отправка секторов в cond response")
-
-	for {
-		select {
-		case item, ok := <-fch:
-			if !ok {
-				// Канал закрыт, выходим из цикла
-				return
-			}
-			receivedSlice = append(receivedSlice, item)
-			fmt.Printf("%s,%+v\n", "отправка", item.SectorID)
-		}
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
 	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	go Fill(ch)
+	ConsumeMessages(ch)
+
+	// Ждем нажатия клавиши Enter перед завершением программы
+	fmt.Println("Press Enter to exit")
+	fmt.Scanln()
 }
