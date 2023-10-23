99758a8db22afd4e06c87a1b042add432ee84902
OEM Configuration (temporary user) <d.shalabayev@kazdream.kz>
2023-10-23 16:08:00 +0600

    Worker

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -9,20 +8,8 @@ import (
 	"github.com/streadway/amqp"
 )
 
-type SectorResponse struct {
-	TaskID     int
-	RoundNo    int
-	SectorID   int
-	CellID     int
-	LAC        int
-	Service    string
-	Timestamp  time.Time
-	ReturnCode int
-	Result     string
-	Reason     string
-}
-
-func Fill() {
+func worker(id int) {
+	// Подключение к RabbitMQ серверу
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
@@ -34,181 +21,54 @@ func Fill() {
 		log.Fatalf("Failed to open a channel: %v", err)
 	}
 	defer ch.Close()
-	queueName := "processed_sectors"
 
-	for i := 0; i < 10; i++ {
-		msg := i
-		fmt.Println("Sending sector", msg+1)
-		response := SectorResponse{
-			TaskID:     msg,
-			RoundNo:    0,
-			SectorID:   msg + 1,
-			CellID:     msg,
-			LAC:        msg,
-			Service:    "service",
-			Timestamp:  time.Now(),
-			ReturnCode: 0,
-			Result:     "result",
-			Reason:     "reason",
-		}
-
-		jsonBody, err := json.Marshal(response)
-		if err != nil {
-			fmt.Println("Failed to marshal JSON:", err)
-			return
-		}
-
-		err = ch.Publish(
-			"",
-			queueName,
-			false,
-			false,
-			amqp.Publishing{
-				ContentType: "application/json",
-				Body:        jsonBody,
-			})
-		if err != nil {
-			fmt.Println("Failed to publish a message:", err)
-			return
-		}
-
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func ConsumeMessages(fch chan SectorResponse) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	// Объявление очереди
+	q, err := ch.QueueDeclare(
+		"processed_sectors", // Название очереди
+		true,                // durable
+		false,               // delete when unused
+		false,               // exclusive
+		false,               // no-wait
+		nil,                 // arguments
+	)
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		log.Fatalf("Failed to declare a queue: %v", err)
 	}
-	defer conn.Close()
 
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("Failed to open a channel: %v", err)
-	}
-	defer ch.Close()
-	queueName := "processed_sectors"
+	// Получение сообщений из очереди
 	msgs, err := ch.Consume(
-		queueName,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
+		q.Name, // Название очереди
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
 	)
 	if err != nil {
 		log.Fatalf("Failed to register a consumer: %v", err)
 	}
 
-	timeoutCount := 0
-	countSectors := 10
-	selectedMessages := make([]SectorResponse, 0)
-loop:
-	for {
-		select {
-		case msg, ok := <-msgs:
-			if !ok {
-				log.Println("Channel closed. Exiting the loop.")
-				return
-			}
+	fmt.Printf("Worker %d started\n", id)
 
-			var sectorResp SectorResponse
-			err := json.Unmarshal(msg.Body, &sectorResp)
-			if err != nil {
-				log.Println("Failed to unmarshal JSON:", err)
-				continue
-			}
-
-			if sectorResp.ReturnCode == 0 && sectorResp.RoundNo == 0 {
-				fmt.Println("Ack in consumer func", sectorResp.SectorID)
-				selectedMessages = append(selectedMessages, sectorResp)
-				fch <- sectorResp
-			} else {
-				fmt.Println("Non-zero ReturnCode, skipping...")
-			}
-
-			if len(selectedMessages) == countSectors {
-				fmt.Println("Received all sectors.")
-				break loop
-			}
-
-		case <-time.After(10 * time.Second):
-			if countSectors == len(selectedMessages) {
-				break loop
-			} else {
-				timeoutCount++
-				if timeoutCount == 10 {
-					break loop
-				}
-			}
-
-		}
+	// Чтение сообщений из очереди
+	for msg := range msgs {
+		msg.Ack(false)
+		fmt.Printf("Worker %d received message: %s\n", id, msg.Body)
+		time.Sleep(time.Second * 1)
 	}
-	close(fch)
 }
 
 func main() {
-	// var foundItems []SectorResponse
-	// var notFoundItems []SectorResponse
-	Locations := make(map[string]int)
-	Locations["132-221"] = 1
-	Locations["132-222"] = 2
-	Locations["132-223"] = 3
-	Locations["132-224"] = 4
-	Locations["132-225"] = 5
-	Locations["132-226"] = 6
-	Locations["132-227"] = 7
-	Locations["132-228"] = 8
-	Locations["132-229"] = 9
-	Locations["132-230"] = 10
-	Locations["132-231"] = 11
-	Locations["132-232"] = 12
-	// отправка в processed_sectors обработанных секторов
-	go Fill()
-
-	var receivedItems []SectorResponse
-	// канал для получения данных из очереди
-	fch := make(chan SectorResponse)
-
-	// утилита для обработки очереди processed_sectors, после нее идет отправка  в бэк
-	go ConsumeMessages(fch)
-
-	// ожидаем получение всех данных из канала перед вызовом sendToCondResponse
-	for item := range fch {
-		fmt.Println("From rabbit sectorId", item.SectorID)
-		receivedItems = append(receivedItems, item)
+	// Запуск нескольких воркеров
+	for i := 1; i <= 3; i++ {
+		go worker(i)
 	}
 
-	var differences []int
+	// Ждем, чтобы воркеры имели время обработать сообщения
+	time.Sleep(5 * time.Second)
 
-	for _, sectorID := range Locations {
-		found := false
-		for _, item := range receivedItems {
-			if sectorID == item.SectorID {
-				found = true
-				break
-			}
-		}
-		if !found {
-			differences = append(differences, sectorID)
-		}
-	}
-	fmt.Println("Различия между Locations и receivedItems:", differences)
-	fmt.Println("Отправка пустых секторов")
-	fmt.Println("Start send to condional response")
-	for _, sectorIDL := range Locations {
-		for _, sectorIdD := range differences {
-			if sectorIDL == sectorIdD {
-				fmt.Println("Сектор: ", sectorIDL)
-			}
-		}
-	}
-
-	// после получения всех данных вызываем sendToCondResponse
-
-	// Ждем нажатия клавиши Enter перед завершением программы
-	fmt.Println("Press Enter to exit")
-
+	// Чтобы программа не завершилась слишком рано
+	fmt.Println("Workers running...")
+	<-make(chan struct{})
 }
---
+++ producer/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+type SectorResponse struct {
+	TaskID     int       `json:"task_id"`
+	RoundNo    int       `json:"round_no"`
+	SectorID   int       `json:"sector_id"`
+	CellID     int       `json:"cell_id"`
+	LAC        int       `json:"lac"`
+	Service    string    `json:"service"`
+	Timestamp  time.Time `json:"timestamp"`
+	ReturnCode int       `json:"return_code"`
+	Result     string    `json:"result"`
+	Reason     string    `json:"reason"`
+}
+
+func Fill(wg *sync.WaitGroup) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+	queueName := "processed_sectors"
+
+	for i := 0; i < 50; i++ {
+		msg := i
+		fmt.Println("Sending sector", msg+1)
+		response := SectorResponse{
+			TaskID:     1,
+			RoundNo:    0,
+			SectorID:   msg + 1,
+			CellID:     msg,
+			LAC:        msg,
+			Service:    "service",
+			Timestamp:  time.Now(),
+			ReturnCode: 0,
+			Result:     "result",
+			Reason:     "reason",
+		}
+
+		jsonBody, err := json.Marshal(response)
+		if err != nil {
+			fmt.Println("Failed to marshal JSON:", err)
+			return
+		}
+
+		err = ch.Publish(
+			"",
+			queueName,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
+			})
+		if err != nil {
+			fmt.Println("Failed to publish a message:", err)
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	for i := 0; i < 50; i++ {
+		msg := i
+		fmt.Println("Sending sector", msg+1)
+		response := SectorResponse{
+			TaskID:     2,
+			RoundNo:    0,
+			SectorID:   msg + 1,
+			CellID:     msg,
+			LAC:        msg,
+			Service:    "service",
+			Timestamp:  time.Now(),
+			ReturnCode: 0,
+			Result:     "result",
+			Reason:     "reason",
+		}
+
+		jsonBody, err := json.Marshal(response)
+		if err != nil {
+			fmt.Println("Failed to marshal JSON:", err)
+			return
+		}
+
+		err = ch.Publish(
+			"",
+			queueName,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
+			})
+		if err != nil {
+			fmt.Println("Failed to publish a message:", err)
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	for i := 0; i < 50; i++ {
+		msg := i
+		fmt.Println("Sending sector", msg+1)
+		response := SectorResponse{
+			TaskID:     3,
+			RoundNo:    0,
+			SectorID:   msg + 1,
+			CellID:     msg,
+			LAC:        msg,
+			Service:    "service",
+			Timestamp:  time.Now(),
+			ReturnCode: 0,
+			Result:     "result",
+			Reason:     "reason",
+		}
+
+		jsonBody, err := json.Marshal(response)
+		if err != nil {
+			fmt.Println("Failed to marshal JSON:", err)
+			return
+		}
+
+		err = ch.Publish(
+			"",
+			queueName,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
+			})
+		if err != nil {
+			fmt.Println("Failed to publish a message:", err)
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go Fill(&wg)
+	wg.Wait()
+}
